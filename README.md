a78bdb4b54ca1f0dabfc1d63ea6af93f15f70536
Alexander Pope <alexander.pope@nrk.no>
2013-02-12 09:38:16 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+A collection of Viewport functions.
+
+## Usage
+```javascript
+var viewport = require('env.viewport');
+
+var width = viewport.getWidth();
+var height = viewport.getHeight();
+```
---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * Retrieve the viewport width
+ * @returns {Number}
+ */
+exports.getWidth = function() {
+	return document.documentElement.clientWidth;
+};
+
+/**
+ * Retrieve the viewport height
+ * @returns {Number}
+ */
+exports.getHeight = function() {
+	return document.documentElement.clientHeight;
+};
